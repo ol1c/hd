da61072e8cbf76fd237e3f1f4d4eafb111cf4e63
ol1c <155087424+ol1c@users.noreply.github.com>
2025-11-23 19:39:47 +0100

    hotfix

--- hd/code/to_bulk.py
+++ hd/code/to_bulk.py
@@ -27,7 +27,7 @@ def save_bulk(out_dir: Path, rooms, exhibitions, exhibits, exhibit_exhibitions, 
     write_bulk(out_dir / "exhibitions.bulk", exhibitions,
                ["exhibition_id", "name", "exhibition_start", "exhibition_end", "room_id"])
     write_bulk(out_dir / "exhibits.bulk", exhibits, ["exhibit_id", "name", "author"])
-    write_bulk(out_dir / "exhibit_exhibitions.bulk", exhibit_exhibitions, ["fk_exhibit_id", "fk_exhibition_id"])
+    write_bulk(out_dir / "exhibit_exhibitions.bulk", exhibit_exhibitions, ["exhibit_id", "exhibition_id"])
     write_bulk(out_dir / "visitors.bulk", visitors, ["visitor_id", "name", "visit_date", "entry_time", "exit_time"])
     write_bulk(out_dir / "exhibition_visits.bulk", exhibition_visits,
                ["visit_id", "visitor_id", "exhibition_id", "entry_time", "exit_time"])
